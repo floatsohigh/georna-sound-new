fb012f159f93d0a7ab153a533f16dcb2bea4d739
floatsohigh <56156188+floatsohigh@users.noreply.github.com>
2019-10-27 00:09:57 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
     {
         resolve: 'gatsby-source-prismic-graphql',
         options: {
-            repositoryName: 'georna-sound', // (REQUIRED, replace with your own)
+            repositoryName: 'georna-sound.prismic.io', // (REQUIRED, replace with your own)
             linkResolver: () => post => `/${post.uid}`,
         }
     },
